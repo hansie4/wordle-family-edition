b32978756177eae8e3c349f8a75cd6e8bc0de2f4
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2023-12-10 18:00:21 -0500

    Initial db

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,15 +1,37 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS word;
+DROP TABLE IF EXISTS attempt;
+DROP TABLE IF EXISTS leaderboard;
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    username TEXT UNIQUE NOT NULL
 );
-CREATE TABLE post (
+CREATE TABLE word (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    author_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
+    word TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES user (id)
+    f_name TEXT NOT NULL
 );
+CREATE TABLE attempt (
+    guesser_id INTEGER PRIMARY KEY,
+    word_id INTEGER,
+    attempt_number INTEGER NOT NULL,
+    attempt_input TEXT NOT NULL,
+    FOREIGN KEY (guesser_id) REFERENCES user (id),
+    FOREIGN KEY (word_id) REFERENCES word (id)
+);
+CREATE TABLE leaderboard (
+    guesser_id INTEGER PRIMARY KEY,
+    correct_guesses INTEGER NOT NULL,
+    FOREIGN KEY (guesser_id) REFERENCES user (id)
+);
+INSERT INTO user (username)
+VALUES ('Hans');
+INSERT INTO user (username)
+VALUES ('Beryl');
+INSERT INTO word (word, body, f_name)
+VALUES ('beryl', 'The best cutie', 'test.png');
+INSERT INTO word (word, body, f_name)
+VALUES ('simba', 'The best cat', 'test.png');
+INSERT INTO word (word, body, f_name)
+VALUES ('hans', 'guy', 'test.png');
