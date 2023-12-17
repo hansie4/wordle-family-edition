17edac73772ba04a737790fa9ea1ce4d1b0857bd
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2023-12-17 13:55:47 -0500

    trying this

--- ui_server.js
+++ ui_server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "app_ui/build")));
 
-app.get("/*", function (req, res) {
+app.get("/ui/*", function (req, res) {
     res.sendFile(path.join(__dirname, "app_ui/build", "index.html"));
 });
 
